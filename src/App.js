533a13184ced73d11258000724f35d837dca12ed
gayoung <rkdud91455@gmail.com>
2023-12-05 23:34:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem  from './components/ExpenseItem'; //컴포넌트 이름은 무조건 대문자로 시작
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get stardfdfed!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
